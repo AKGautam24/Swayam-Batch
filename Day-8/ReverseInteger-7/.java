f0075ce6c4c54309bc94fa703bb355fe6f3a6ce1
AKGautam24 <anujgautam03012018@gmail.com>
2025-09-19 18:34:20 +0530

    Create .java

---
+++ Day-8/ReverseInteger-7/.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int reverse(int x) {
+        int result =0;
+        int sign=x<0 ? -1:1;
+        x=Math.abs(x);
+
+        while(x>0) {
+            int digit=x%10;
+            if(result> Integer.MAX_VALUE / 10 || 
+                (result == Integer.MAX_VALUE / 10 && digit > 7)) {
+                return 0;
+                }
+                 result = result * 10 + digit;
+                 x/=10;
+        }
+        return sign * result;
+        
+    }
+}
